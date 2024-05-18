4700ae2ca13725e85f31ab10e4f34983a2bcd282
Festere <trail.betel6530@eagereverest.com>
2024-05-19 01:07:17 +0200

    v0.0.4

--- Locker.ino
+++ Locker.ino
@@ -1,22 +1,22 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <Stepper.h>
+#include <AccelStepper.h>
 #include <Keypad.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 // Defines the number of steps per rotation
-Stepper myStepper = Stepper(1000, 13,12,11,10);
+AccelStepper stepper;
 
 // password per lock
-const char lock_1_pass = "d5ef4ecfee2a85a668be66ca615d2b2d"; // Hash of "1A56B"
-const char lock_2_pass = "b51fc8660eba0dd914d96510642fad92"; // Hash of "584CA"
-
-const char plainText = "d5ef4ecfee2a85a668be66ca615d2b2d";
+String lock_1_pass = "1A56B"; // Hash of "1A56B"
+String lock_2_pass = "584CA"; // Hash of "584CA"
 
 // lock engaged (flase = open / true = closed)
 bool lock_1 = true;
 bool lock_2 = true;
+
+String input = "";
 
 // Configuration du clavier numérique dans une matrice
 const byte ROWS = 4;
@@ -27,54 +27,62 @@ char keys[ROWS][COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-
-byte rowPins[ROWS] = {13, 12, 27, 33}; // Connecter les lignes du clavier numérique aux broches de la carte wifi
-byte colPins[COLS] = {4, 5, 18, 19 }; // Connecter les colonnes du clavier numérique aux broches de la carte wifi
-
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+byte rowPins[ROWS] = {22, 21, 16, 7};
+byte colPins[COLS] = {6, 5, 12, 14};
+Keypad customKeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
 
   lcd.begin(16,2);
   lcd.init();
   lcd.backlight();
+
+  stepper.setMaxSpeed(100);
 }
 
 
 void loop() {
-  
   lcd.setCursor(0,0);
   lcd.print("Enter your code:");
+  char customKey = customKeypad.getKey();
+  if (customKey){
+    // Add the key to the input string
+    input += customKey;
+    Serial.print(customKey);
+    // Check if the input length is 5
+    if (input.length() == 5){
+      Serial.print(" -> Full input: ");
+      Serial.println(input);
+      if (lock_1 == true && lock_1_pass == input) {
+        digitalWrite(0, HIGH);
+        Serial.println("Lock 1 has been opened");
 
-  if (lock_1 == true && lock_1_pass == plainText) {
-    // turn on the light
-    digitalWrite(2, HIGH);
+        stepper.setAcceleration(20);
+        stepper.moveTo(500);
+        delay(60000);
+        input = "";
+      }
+      else if (lock_2 == true && lock_2_pass == input) {
+        digitalWrite(1, HIGH);
+        Serial.println("Lock 2 has been opened");
 
-    // use the motor to open the lock
-    myStepper.setSpeed(10);
-    myStepper.step(100);
-    delay(2000);
+        stepper.setAcceleration(20);
+        stepper.moveTo(500);
+        delay(60000);
+        input = "";
+      }
+      else {
+        digitalWrite(0, LOW);
+        digitalWrite(1, LOW);
+        Serial.println("A wrong attempts has occurred");
+        input = "";
+      }
+    }
   }
-  else {
-    digitalWrite(2, LOW);
-  }
-  
-
-  if (lock_2 == true && lock_2_pass == plainText) {
-    // turn on the light
-    digitalWrite(3, HIGH);
-
-    // use the motor to open the lock
-    myStepper.setSpeed(10);
-    myStepper.step(2038);
-    delay(200);
-  }
-  else {
-    digitalWrite(3, LOW);
-  }
+  delay(200);
 }
